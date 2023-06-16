18487448c6136a086fd95bce50ad23a80e3b36cb
Davi Costa <davifncosta@gmail.com>
2023-06-16 13:40:19 -0300

    START TO ADD FUNCTION COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import "./App.css";
+
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.jsx";
 import SearchBox from "./components/search-box/search-box.jsx";
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
+  const  onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+    
         this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+          return { searchField };
+        });
+      };
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonster = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div>
-        <header className="header mt-[2rem]">
+  return (
+    <div>
+         <header className="header mt-[2rem]">
           <h1 className="text-[3rem] font-bold">Monster Roledex 👾</h1>
           <div className="h-[0.2rem] w-[30rem] bg-[#363636] rounded mx-[auto] transform -translate-y-1  min450:hidden"></div>
           <SearchBox
@@ -48,8 +24,58 @@ class App extends Component {
         </header>
         <CardList monsters={filteredMonster} />
       </div>
-    );
-  }
+  )
 }
 
-export default App;
+export default App
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonster = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div>
+//         <header className="header mt-[2rem]">
+//           <h1 className="text-[3rem] font-bold">Monster Roledex 👾</h1>
+//           <div className="h-[0.2rem] w-[30rem] bg-[#363636] rounded mx-[auto] transform -translate-y-1  min450:hidden"></div>
+//           <SearchBox
+//             onChangeHandler={onSearchChange}
+//             placeholder="Find your monster"
+//           />
+//         </header>
+//         <CardList monsters={filteredMonster} />
+//       </div>
+//     );
+//   }
+// }
+
