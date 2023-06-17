12d761577cf4313dbb5052b4e5cef15aade7db31
Davi Costa <davifncosta@gmail.com>
2023-06-17 10:12:21 -0300

    ReactDOM v18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import React from "react"
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
